5027f02a9819fdeb98ca53a947757b7b8e2aa0cc
CFatAWS <fei.chen@dena.com>
2015-12-23 11:31:17 +0800

    set `cat_name` column's default value to 'exact_match'

--- application/sql/001.add_column_cat_to_wechat_reply.sql
+++ application/sql/001.add_column_cat_to_wechat_reply.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `wechat_reply` ADD COLUMN `cat_name` VARCHAR(64) NOT NULL;
+ALTER TABLE `wechat_reply` ADD COLUMN `cat_name` VARCHAR(64) NOT NULL DEFAULT 'exact_match';
 
 update `wechat_reply` set `cat_name` = 'exact_match';
